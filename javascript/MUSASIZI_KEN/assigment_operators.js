16642ac7b2ea43a663f309cea7534b51e875b8bb
Musasizi Kenneth <kennymusasizi@hotmail.com>
2023-02-10 10:10:33 +0300

    more about functions

---
+++ javascript/MUSASIZI_KEN/assigment_operators.js
@@ -0,0 +1,11 @@
+/* ASSIGNMENT OPERATORS
+Assignment Operators
+Addition Assignment Operators       +=
+Subtraction Assignment Operators    -=
+Multiplication Assignment Operators *=
+Division Assignment Operators       /=
+Remainder Assignment Operators      %=
+Exponential  Assignment Operators   **=
+
+*/
+
