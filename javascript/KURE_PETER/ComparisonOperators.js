57ae71f4653ee4daa1b4bd46f837ce4839aa1405
Peter Kure <peterkure256@gmail.com>
2023-02-12 00:23:09 +0300

    First Commit: Operators

---
+++ javascript/KURE_PETER/ComparisonOperators.js
@@ -0,0 +1,31 @@
+// Implementation of Comparison operators in Javascript
+let a = 11;
+let b = 21;
+
+// Equal to operator(==):
+let equal = b == a;
+console.log(equal);
+
+// Not equal to operator (!=):
+let NotEqual = b != a;
+console.log(NotEqual);
+
+//  Greater than operator (>):
+let greater = b>a;
+console.log(greater);
+
+//  Less than operator (<):
+let less = a<b;
+console.log(less);
+
+//  Greater than or equal to operator (>=):
+a=12;
+b=13;
+greaterOrEqual = b>=a;
+console.log(greaterOrEqual);
+
+//  Less than or equal to operator (<=):
+a=14;
+b=13;
+lessOrEqual = a<=b;
+console.log(lessOrEqual);
