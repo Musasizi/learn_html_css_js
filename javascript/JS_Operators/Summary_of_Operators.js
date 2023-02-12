4850fd71f4d604825c41c751cb82ce25b16bf074
Ashaba Jasper <106011252+AshabaJasper@users.noreply.github.com>
2023-02-12 18:50:12 +0300

    Update_Operators

---
+++ javascript/JS_Operators/Summary_of_Operators.js
@@ -0,0 +1,60 @@
+// Initializing variables
+let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+let str1 = "Hello";
+let str2 = "World";
+let bool1 = true;
+let bool2 = false;
+
+// Arithmetic operators
+let sum = num1 + num2;
+let difference = num2 - num1;
+let product = num1 * num2;
+let quotient = num2 / num1;
+let modulus = num2 % num1;
+
+// Assignment operators
+num3 += num1;
+num3 -= num2;
+num3 *= num1;
+num3 /= num2;
+num3 %= num1;
+
+// Comparison operators
+let isEqual = num1 == num2;
+let isNotEqual = num1 != num2;
+let isStrictEqual = num1 === num2;
+let isStrictNotEqual = num1 !== num2;
+let isGreaterThan = num1 > num2;
+let isLessThan = num1 < num2;
+let isGreaterThanOrEqualTo = num1 >= num2;
+let isLessThanOrEqualTo = num1 <= num2;
+
+// Logical operators
+let andResult = bool1 && bool2;
+let orResult = bool1 || bool2;
+let notResult = !bool1;
+
+// String concatenation
+let concatResult = str1 + " " + str2;
+
+// Outputting results to the console
+console.log("Sum: ", sum);
+console.log("Difference: ", difference);
+console.log("Product: ", product);
+console.log("Quotient: ", quotient);
+console.log("Modulus: ", modulus);
+console.log("Num3 after arithmetic operations: ", num3);
+console.log("Is equal: ", isEqual);
+console.log("Is not equal: ", isNotEqual);
+console.log("Is strict equal: ", isStrictEqual);
+console.log("Is strict not equal: ", isStrictNotEqual);
+console.log("Is greater than: ", isGreaterThan);
+console.log("Is less than: ", isLessThan);
+console.log("Is greater than or equal to: ", isGreaterThanOrEqualTo);
+console.log("Is less than or equal to: ", isLessThanOrEqualTo);
+console.log("And result: ", andResult);
+console.log("Or result: ", orResult);
+console.log("Not result: ", notResult);
+console.log("String concatenation result: ", concatResult);
