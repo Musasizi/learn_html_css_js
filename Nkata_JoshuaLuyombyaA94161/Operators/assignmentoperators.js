8c5bfb9c7d7153633f8ca4a86e06dee425112d3b
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2023-02-12 11:36:14 +0300

    Add files via upload

---
+++ Nkata_JoshuaLuyombyaA94161/Operators/assignmentoperators.js
@@ -0,0 +1,54 @@
+/*Assignment operators */
+let l = 35;
+let n = 75;
+
+/*Addition Assignment Operators += */
+console.log(l+=n);
+
+/*Subtraction Assignment Operators -=*/
+console.log(l-=n);
+
+/*Multiplication Assignment Operators *= */
+console.log(l*=n);
+
+/*Division Assignment Operators /= */
+console.log(l/=n);
+
+/*Remainder Assignment Operators %=*/
+console.log(l+=n);
+
+/*Exponential  Assignment Operators **= */
+console.log(l**=n);
+
+/*Comparison_operators*/
+
+/*less than <*/ 
+if (l<n){
+    console.log("TRUE");
+}
+
+/*greater than <*/
+if (l>n){
+    console.log("TRUE");
+}
+
+/*greater than or equal to >= */
+if (l>=n){
+    console.log("TRUE");
+}
+
+/*less than or equal to <= */
+if (l<=n){
+    console.log("TRUE");
+}
+
+/*Bitwise_operators*/
+/*AND*/
+console.log(l&n);
+/*OR*/
+console.log(l|n);
+/*NOT*/
+console.log(~n);
+
+
+
