890c54f2bcbb35d27113138e43f4df25b95a4f7f
HassanBahati <mukisabahati@gmail.com>
2023-02-12 22:42:03 +0300

    feat: add arithmentic operators

---
+++ javascript/HASSAN_BAHATI/arithmetic_operators.js
@@ -0,0 +1,20 @@
+/**ARITHMENTIC OPERATORS */
+
+//ADDITION OPERATOR
+/**returns the sum of the operands */
+let a = 3;
+let x = (100 + 50) * a;
+
+console.log(a + x); // output: 453
+
+// SUBTRACTION OPERATOR
+/**returns the difference of the operands */
+console.log(a - x); // output: -447
+
+//MULTIPLICATION
+/**returns the product of the operands */
+console.log(a * x); // output: 1350
+
+//DIVISION OPERATOR
+/** returns quotient of the operands */
+console.log(a / x); // output: 0.006666666666666667
