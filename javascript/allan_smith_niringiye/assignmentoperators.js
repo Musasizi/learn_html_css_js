2f8245f748ffa7bdd37eedee7154f124a0c0267f
trizey14 <trizey14@gmail.com>
2023-02-12 15:48:32 +0300

    Operators assignment

---
+++ javascript/allan_smith_niringiye/assignmentoperators.js
@@ -0,0 +1,79 @@
+/* 
+ASSIGNMENT OPERATORS
+Assignment Operators
+Addition Assignment Operators       +=
+Subtraction Assignment Operators    -=
+Multiplication Assignment Operators *=
+Division Assignment Operators       /=
+Division Assignment Operators       /=
+Exponential  Assignment Operators   **=
+*/
+
+// The Assignment Operator (=) assigns a value to a variable
+// For example
+let b = 10; 
+// This assigns b to the value 10
+
+// More examples
+// Assign the value 5 to x
+let x = 5;
+// Assign the value 2 to y
+let y = 2;
+// Assign the value x + y to z:
+let z = x + y;
+
+// 2. Addition Assignment Operators       +=
+    // The "+=" operator in Javascript is called the Addition Assignment Operator.
+//  It adds the right operand to the left operand and then assigns the result to the left operand.
+    //   For example:
+
+var c = 10; 
+c += 5; 
+
+
+
+// 3. Subtraction Assignment Operators    -=
+    // The subtraction assignment operator "-" in Javascript is used to subtract a value from a variable and assign the result back to the same variable.
+    // The subtraction assignment operator can be represented as "-=".
+
+    let r = 10;
+r -= 5;
+console.log(r); // Output: 5
+
+// 4. Subtraction Assignment Operators    -=
+// The subtraction assignment operator "-" in Javascript is used to subtract a value from a variable.
+//  The subtraction assignment operator "-" can be written as "-" followed by "=". 
+//  This operator performs the subtraction of the right-hand operand from the left-hand operand and assigns the result back to the left-hand operand.
+
+// For example:
+let k = 100;
+k -= 50;
+console.log(k); // Output: 50
+
+    // 5. Multiplication Assignment Operators *=
+// The multiplication assignment operator "*=" in Javascript is used to multiply a value to a variable.
+//  The operator performs the multiplication of the right-hand operand with the left-hand operand and assigns the result back to the left-hand operand.
+
+// For example:
+let p = 10;
+p *= 5;
+console.log(x); // Output: 50
+
+
+    // 6. Division Assignment Operators       /=
+// The division assignment operator "/=" in Javascript is used to divide a variable by a value.
+//  The operator performs the division of the left-hand operand by the right-hand operand and assigns the result back to the left-hand operand.
+//  For example:
+ let u = 50;
+u /= 5;
+console.log(u); // Output: 10
+
+// 8. Exponential  Assignment Operators   **=
+// The exponential assignment operator "**=" in Javascript is used to raise a variable to the power of a value. 
+// The operator performs the exponentiation of the left-hand operand by the right-hand operand and assigns the result back to the left-hand operand.
+
+// For example:
+
+let v = 2;
+v **= 3;
+console.log(v); // Output: 8
