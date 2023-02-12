efbe5e8051cf147c1415b06d9b06005e98a0a7b4
HassanBahati <mukisabahati@gmail.com>
2023-02-12 17:34:32 +0300

    chore: initial commit

---
+++ javascript/HASSAN_BAHATI/bitwise_operators.js
@@ -0,0 +1,3 @@
+/* BITWISE OPERATORS
+
+*/
---
+++ javascript/HASSAN_BAHATI/comparison_operators.js
@@ -0,0 +1,10 @@
+/* COMPARISON OPERATORS
+Addition Assignment Operators       +=
+Subtraction Assignment Operators    -=
+Multiplication Assignment Operators *=
+Division Assignment Operators       /=
+Remainder Assignment Operators      %=
+Exponential  Assignment Operators   **=
+
+
+*/
