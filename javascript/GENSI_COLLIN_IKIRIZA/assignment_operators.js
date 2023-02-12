6b23b159b4eacb65717e5b22294a461d076d0a41
Gensi Collin <99279784+Ikiriza@users.noreply.github.com>
2023-02-12 15:50:57 +0300

    operators

---
+++ javascript/GENSI_COLLIN_IKIRIZA/assignment_operators.js
@@ -0,0 +1,17 @@
+//The Simple Assignment Operators 
+
+//Examples
+
+var x = 10;
+var x = 10;
+x += 5; // x will be 15 after this operation
+var x = 10;
+x -= 5; // x will be 5 after this operation
+var x = 10;
+x *= 5; // x will be 50 after this operation
+var x = 10;
+x /= 5; // x will be 2 after this operation
+var x = 10;
+x %= 3; // x will be 1 after this operation
+var x = 10;
+x **= 5;
---
+++ javascript/GENSI_COLLIN_IKIRIZA/comparison_operators.js
@@ -0,0 +1,29 @@
+// Equality (==): This operator tests for equality between two values without considering the type of the operands.
+// javascript
+
+var x = 10;
+var y = "10";
+console.log(x == y);  // true
+
+let x = 10;
+let y = "10";
+console.log(x === y);  // false
+
+let x = 10;
+let y = 20;
+console.log(x != y);  // true
+
+let x = 10;
+let y = "10";
+console.log(x !== y);  // true
+
+let x = 10;
+let y = 20;
+console.log(x > y);  // false
+console.log(x < y);  // true
+
+let x = 10;
+let y = 20;
+console.log(x >= y);  // false
+console.log(x <= y);  // true
+
