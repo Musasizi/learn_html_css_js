d60062e84e2a76e5714f0d21a60845e2fe986da7
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2023-02-13 21:30:12 +0300

    Add files via upload

---
+++ javascript/DAPHINE_KAMUSIIME/Arithmetic.js
@@ -0,0 +1,41 @@
+// ARITHMETIC OPERATORS
+
+// AdditioN " + "
+let count = 10
+// adding a new value to a variable 
+count + 1
+
+// Subtraction " - " 
+let count1 = 10
+// subtracting a new value from a variable 
+count1 - 1
+
+// Multiplication operator *
+let count2 = 10
+// multiplying a new value with a variable 
+count2 * 2
+
+// Division operator /
+let count3 = 10
+// dividing an already existing variable by a new value 
+count3 / 2
+
+// Remainder operator %
+let count4 = 10
+// dividing a variable by a new value and return the remainder
+count4 % 3
+
+// Increment operator ++
+let count5 = 10
+// adding one to a variable 
+count5 ++
+
+// Decrement operator --
+let count6 = 10
+//subtracting one from a variable 
+count6 --
+
+// Exponential operator **
+let count7 = 10
+// getting the variable to the power another number 
+count7 ** 2
