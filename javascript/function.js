2bd7aa7f7d48a7ecacf3fa11ee23d7ba4294ae30
Musasizi Kenneth <kennymusasizi@hotmail.com>
2023-02-22 09:51:03 +0300

    feat: functions

---
+++ javascript/function.js
@@ -0,0 +1,18 @@
+// Functions
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// console.log(addNumbers(45, 50));
+
+// Arrow functions
+
+const subtractNumbers = (num1, num2) => {
+    return num1 - num2
+}
+// console.log(subtractNumbers(45, 50));
+
+const multiplyNumbers = (num1, num2) => num1 * num2;
+
+console.log(multiplyNumbers(45, 50));
