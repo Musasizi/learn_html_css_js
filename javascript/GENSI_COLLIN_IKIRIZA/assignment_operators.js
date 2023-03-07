dca9d328fd4172dd15beb1ea1bc2ed772dd0e0e4
Gensi Collin <99279784+Ikiriza@users.noreply.github.com>
2023-03-07 13:03:05 +0300

    Assignment on loops javascript

--- javascript/GENSI_COLLIN_IKIRIZA/assignment_operators.js
+++ javascript/GENSI_COLLIN_IKIRIZA/assignment_operators.js
@@ -1,17 +1,43 @@
 //The Simple Assignment Operators 
 
 //Examples
+// The assignment operator in JavaScript is the = symbol, which is used to assign a value to a variable. Here's an example:
+var x;
+x = 10;
+console.log(x); // Output: 10
+// The "+=" assignment operator in JavaScript is used to add the right operand to the left operand and then assign the result to the left operand.
+// Here is an example:
+var x = 10;
+x += 5;
+console.log(x); // Output: 15
 
-var x = 10;
-var x = 10;
-x += 5; // x will be 15 after this operation
-var x = 10;
-x -= 5; // x will be 5 after this operation
-var x = 10;
-x *= 5; // x will be 50 after this operation
-var x = 10;
-x /= 5; // x will be 2 after this operation
-var x = 10;
-x %= 3; // x will be 1 after this operation
-var x = 10;
-x **= 5;
+// In JavaScript, the subtraction assignment operator -= is used to subtract a value from a variable and reassign the result back to the same variable. Here's an example of using the -= operator:
+let x = 10;
+x -= 5;
+console.log(x); // Output: 5
+
+// In JavaScript, the * (multiplication) assignment operator is used to multiply the value of a variable by the value on its right, and then assigns the result to the variable. Here's an example:
+var num = 10;
+num *= 5;
+console.log(num);  // output: 50
+
+// In JavaScript, the / (division) assignment operator is used to divide the value of a variable by the value on its right, and then assigns the result to the variable. Here's an example:
+let num = 100;
+num /= 5;
+console.log(num);  // output: 20
+
+// The modulo operator in JavaScript is represented by the % symbol and it returns the remainder of dividing the left operand by the right operand. For example, if we have 10 % 3, the result will be 1 because 3 goes into 10 three times with a remainder of 1.
+// Here's a simple example of using the modulo operator in JavaScript:
+let number = 10;
+let divisor = 3;
+let remainder = number % divisor;
+console.log(remainder); // Output: 1
+
+// The "exponentiation" operator in JavaScript is represented by the ** symbol and it returns the result of raising the left operand to the power of the right operand.
+// Here's a simple example of using the exponentiation operator in JavaScript:
+let base = 2;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result); // Output: 8
+
+
